468506ffd5a9b444359ec78a48b8dbd9e515c36d
Julian <julian.costinea@gmail.com>
2021-10-12 01:57:15 +0200

    added UI to videogames page

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,6 +1,7 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import SideDrawerContext from "../../store/SideDrawerContext";
 import Link from "next/link";
+import * as Icons from '../../components/UI/Icons/Icons';
 
 import classes from "./Logo.module.css";
 
@@ -8,25 +9,12 @@ const logo = (props) => {
   const sideDrawerCtx = useContext(SideDrawerContext);
   return (
     <div className={classes.Logo} style={{ cursor: "pointer" }}>
-      <Link href="/"  passHref>
-        <div onClick={sideDrawerCtx.hideSideDrawer} className={classes.LogoContainer}>
-          <svg
-            display="inline-block"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            width="40"
-            height="30"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
+      <Link href="/" passHref>
+        <div
+          onClick={sideDrawerCtx.hideSideDrawer}
+          className={classes.LogoContainer}
+        >
+          {Icons.SearchIcon}
           <p>
             <strong>|SpotMyNext</strong>
           </p>
--- pages/video-games/index.js
+++ pages/video-games/index.js
@@ -1,7 +1,37 @@
 import classes from "./video-games.module.css";
 
+import * as Icons from "../../components/UI/Icons/Icons";
+import React from "react";
+
 const VideoGames = () => {
-  return <h1 className={classes.title}>Video games page</h1>;
+  return (
+    <React.Fragment>
+      <h1>Let's find your next video game</h1>
+      <div className={classes.wrap}>
+        <div className={classes.search}>
+          <input
+            type="text"
+            className={classes.searchTerm}
+            placeholder="Spot my next videogame"
+          />
+          <button type="submit" className={classes.searchButton}>
+            {Icons.SearchIcon}
+          </button>
+        </div>
+      </div>
+      <div className={classes.reccomendations}>
+        <div className={classes.reccomendation}>
+          <p>videogame</p>
+        </div>
+        <div className={classes.reccomendation}>
+          <p>videogame</p>
+        </div>
+        <div className={classes.reccomendation}>
+          <p>videogame</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default VideoGames;
