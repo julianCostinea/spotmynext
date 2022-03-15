41126832b579358931079ee3cff072988a69e4ab
julian <julian.costinea@gmail.com>
2022-03-15 17:42:12 +0100

    stored jwt in cookie

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 const jwt = require("jsonwebtoken");
 
-export function middleware(req: NextRequest) {
+export function middleware(req: NextRequest, res:NextResponse) {
   // Add the user token to the response
   console.log("MIDDLEWARE WORKING");
+  
   const payload = {
     name: "Jimmy"
   }
-  const token = jwt.sign(payload, "secret");
-  console.log(token);
-  
+  const token = jwt.sign(payload, "secret", {expiresIn: '1h'});
+  return NextResponse.next().cookie("secret", token);
 }
